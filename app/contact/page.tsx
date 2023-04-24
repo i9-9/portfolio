97698cef765bd9d72d6cb5386ab994b0e42b0e64
c7-studio <c7studio.www@gmail.com>
2023-04-24 14:07:40 -0300

    fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,23 +13,23 @@ const page = () => {
       </h3>
       <div className="grid md:grid-cols-3 text-center my-10 gap-6 mx-1">
         <Link href='mailto:ivannevares9@gmail.com'>
-          <div className="bg-verde h-fit py-20 hover:bg-[#5226AA] hover:text-[#ADE252] transition-all duration-700">
+          <div className=" bg-verde h-fit py-20 hover:bg-[#5226AA] hover:text-[#ADE252] transition-all duration-700">
             <h4 className="text-3xl md:text-7xl font-bold">EMAIL</h4>
           </div>
         </Link>
         <Link href='https://www.behance.net/ivan_nevares'>
-          <div className="bg-verde h-fit py-20 hover:bg-[#5226AA] hover:text-[#ADE252] transition-all duration-700">
+          <div className=" bg-verde h-fit py-20 hover:bg-[#5226AA] hover:text-[#ADE252] transition-all duration-700">
             <h4 className="text-3xl md:text-7xl font-bold">BEHANCE</h4>
           </div>
         </Link>
         <Link href='https://github.com/i9-9'>
-          <div className="bg-verde h-fit py-20 hover:bg-[#5226AA] hover:text-[#ADE252] transition-all duration-700">
+          <div className=" bg-verde h-fit py-20 hover:bg-[#5226AA] hover:text-[#ADE252] transition-all duration-700">
             <h4 className="text-3xl md:text-7xl font-bold">GITHUB</h4>
           </div>
         </Link>
       </div>
       <div>
-        <button className="border border-verde p-2 mx-1 text-verde text-3xl hover:bg-verde hover:text-violeta transition transition-all">
+        <button className="border border-verde p-2 mx-1 text-verde text-3xl hover:bg-verde hover:text-violeta drop-shadow transition transition-all">
           <a
             href="/cv_inevares_front.pdf"
             target="_blank"
